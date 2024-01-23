0b2a63f87007b7b5bbc17f1e32474470cfcd3c60
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2024-01-23 14:32:44 -0800

    ml

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hi world')
+const express=require('express')
+const app=express()
+const cors=require('cors')
+const port=4200
+const bodyparser=require('body-parser')
+app
+app.use(cors({
+    origin:'*'
+}))
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.get('/', (req,res)=>{
+    return res.send('homepage')
+})
+
+app.listen(port,()=>{
+    console.log('server running fine')
+})
